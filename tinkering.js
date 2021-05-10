360983ea841d3cf0c3ba80bd47636a791ecdfbc9
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-05-10 20:57:34 +0000

    Some More Fundamental Coding Practice

--- tinkering.js
+++ tinkering.js
@@ -224,10 +224,10 @@ if ((shirtWidth > 17.99 && shirtWidth < 20) && (shirtLength > 27.99 && shirtLeng
  */
 
 // change the value of `education` to test your code
-var education = 'no high school diploma';
+//var education = 'no high school diploma';
 
 // set the value of this based on a person's education
-var salary = 0;
+//var salary = 0;
 
 // your code goes here
 
@@ -239,7 +239,7 @@ a Master 's degree earned an average of $69,732/year,
 a Professional degree earned an average of $89, 960 / year,
     and a Doctoral degree earned an average of $84, 396 / year.*/
 
-
+/*
 switch (education) {
     case "no high school diploma":
         salary = 25636;
@@ -273,8 +273,13 @@ switch (education) {
         break;
 }
 console.log("In 2015, a person with " + education + " earned an average of $" + salary.toLocaleString("en-US") + "/year.");
+*/
 
 
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 /*const whichSchool = function(age) {
 
     if (age < 13) {
@@ -288,3 +293,135 @@ console.log("In 2015, a person with " + education + " earned an average of $" + 
 }
 
 whichSchool(15);*/
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+ * Programming Quiz: JuliaJames (4-1)
+ */
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `x` with a starting value of `1`
+ * - Your code should include a `while` loop
+ * - Your `while` loop should have a stop condition
+ * - Your code should use a conditional statement
+ * - Your code should increment `x` by `1` each time the loop executes
+ * - Your code should produce the expected output
+ * - Your code should not be empty
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+
+/*var x = 1;
+
+while (x <= 20) {
+    // check divisibility
+    if ((x % 3 === 0) && (x % 5 === 0)) { console.log("JuliaJames"); } else if (x % 3 === 0) { console.log("Julia"); } else if (x % 5 == 0) { console.log("James"); } else { console.log("Number Not Divisible By 3 Or 5: " + x); }
+    x++;
+    // print Julia, James, or JuliaJames
+    // increment x
+}*/
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+ * Programming Quiz: 99 Bottles of Juice (4-2)
+ *
+ * Use the following `while` loop to write out the song "99 bottles of juice".
+ * Log the your lyrics to the console.
+ *
+ * Note
+ *   - Each line of the lyrics needs to be logged to the same line.
+ *   - The pluralization of the word "bottle" changes from "2 bottles" to "1 bottle" to "0 bottles".
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `num` with a starting value of `99`
+ * - Your code should include a `while` loop
+ * - Your `while` loop should have a stop condition
+ * - Your code should produce the expected output, as explained above
+ * - Your code should not be empty
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+
+//99 bottles of juice on the wall! 99 bottles of juice! Take one down, pass it around...
+
+/*var num = 99;
+while (num > 0) {
+    if (num > 2)
+        console.log(num + " bottles of juice on the wall! " + num + " bottles of juice! Take one down, pass it around... " + (num - 1) + " bottles of juice on the wall!");
+    else if (num === 2)
+        console.log(num + " bottles of juice on the wall! " + num + " bottles of juice! Take one down, pass it around... " + (num - 1) + " bottle of juice on the wall!");
+    else if (num === 1)
+        console.log(num + " bottle of juice on the wall! " + num + " bottle of juice! Take one down, pass it around... " + (num - 1) + " bottles of juice on the wall!");
+    num--;
+
+    // check value of num
+    // print lyrics using num
+    // don't forget to check pluralization on the last line!
+    // decrement num
+}*/
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+ * Programming Quiz: Countdown, Liftoff! (4-3)
+ * 
+ * Using a while loop, print out the countdown output above.
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should include a `while` loop
+ * - Your `while` loop should have a stop condition
+ * - Your code should produce the expected output, as explained above
+ * - Your code should not be empty
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+
+/*let x = 60;
+
+while (x >= 0) {
+
+    if (x === 50) {
+        console.log("Orbiter transfers from ground to internal power");
+    } else if (x === 31) {
+        console.log("Ground launch sequencer is go for auto sequence start");
+    } else if (x === 16) {
+        console.log("Activate launch pad sound suppression system");
+    } else if (x === 10) {
+        console.log("Activate main engine hydrogen burnoff system");
+    } else if (x === 6) {
+        console.log("Main engine start");
+    } else if (x === 0) {
+        console.log("Solid rocket booster ignition and liftoff!");
+    } else {
+        console.log("(T -" + x + " seconds)");
+    }
+    x--;
+}*/
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
