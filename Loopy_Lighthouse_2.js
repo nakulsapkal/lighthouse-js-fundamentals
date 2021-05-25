e3e8e997732bb051b9796c57d75a32c233b6c8d3
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-05-25 02:15:26 +0000

    code refactoring

--- Loopy_Lighthouse_2.js
+++ Loopy_Lighthouse_2.js
@@ -1,17 +1,16 @@
-//loopyLighthouse(range, multiples, words)
-
-
 const loopyLighthouse = function(range, multiples, words) {
 
     for (let i = range[0]; i <= range[1]; i++) {
-        if ((i % multiples[0] === 0) && (i % multiples[1] === 0))
-            console.log(`${words[0]}${words[1]}`);
-        else if (i % multiples[0] === 0)
-            console.log(`${words[0]}`);
-        else if (i % multiples[1] === 0)
-            console.log(`${words[1]}`);
-        else
-            console.log(i);
+        let str = "";
+
+        if (i % multiples[0] === 0)
+            str += words[0];
+        if (i % multiples[1] === 0)
+            str += words[1];
+        if (str === "")
+            str = i;
+
+        console.log(str);
     }
 };
 
